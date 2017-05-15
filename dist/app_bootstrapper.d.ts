b4ffba3877684a5bd459e3d1d5f631b81b068fe9
5ebastianMeier <sebastian.meier@5minds.de>
2017-05-15 14:54:14 +0200

    Initial commit

---
+++ dist/app_bootstrapper.d.ts
@@ -0,0 +1,22 @@
+import { DependencyInjectionContainer as Container } from 'addict-ioc';
+import { ExtensionBootstrapper } from '@process-engine-js/bootstrapper';
+import { IFactory } from '@process-engine-js/core_contracts';
+export declare const extensionDiscoveryTag: string;
+export declare class AppBootstrapper {
+    private _container;
+    private _appRoot;
+    private _env;
+    private _configPath;
+    private _extensionBootstrapper;
+    private _isInitialized;
+    constructor(_container: Container, extensionBootstrapperLazy: IFactory<ExtensionBootstrapper>, appRoot?: string);
+    protected isInitialized: boolean;
+    readonly appRoot: string;
+    protected readonly container: Container;
+    protected readonly extensionBootstrapper: ExtensionBootstrapper;
+    readonly env: string;
+    readonly configPath: string;
+    protected initializeLogging(): void;
+    initialize(): Promise<void>;
+    start(): Promise<void>;
+}
---
+++ src/app_bootstrapper.ts
@@ -0,0 +1,84 @@
+import {DependencyInjectionContainer as Container} from 'addict-ioc';
+import {configureAddictIocWithNconf as configureAddictIocWithNconf} from 'addict-ioc-nconf';
+import * as bluebirdPromise from 'bluebird';
+import * as path from 'path';
+import * as util from 'util';
+import {ExtensionBootstrapper} from '@process-engine-js/bootstrapper';
+import {IFactory} from '@process-engine-js/core_contracts';
+
+import * as createLogger from 'loggerhythm';
+const logger = createLogger('bootstrapper');
+
+export const extensionDiscoveryTag: string = 'extension';
+
+export class AppBootstrapper {
+
+  private _container: Container;
+  private _appRoot: string = process.cwd();
+  private _env: string = process.env.NODE_ENV || 'development';
+  private _configPath: string = process.env.CONFIG_PATH || this._appRoot;
+  private _extensionBootstrapper: ExtensionBootstrapper;
+  private _isInitialized: boolean = false;
+
+  constructor(_container: Container,
+              extensionBootstrapperLazy: IFactory<ExtensionBootstrapper>,
+              appRoot?: string) {
+    this._container = _container;
+    if (appRoot) {
+      this._appRoot = path.normalize(appRoot);
+    }
+    this._extensionBootstrapper = extensionBootstrapperLazy([extensionDiscoveryTag]);
+    this.container.registerObject('appBootstrapper', this);
+
+  }
+
+  protected get isInitialized(): boolean {
+    return this._isInitialized;
+  }
+
+  protected set isInitialized(initialize: boolean) {
+    this._isInitialized = initialize;
+  }
+
+  public get appRoot(): string {
+    return this._appRoot;
+  }
+
+  protected get container(): Container {
+    return this._container;
+  }
+
+  protected get extensionBootstrapper(): ExtensionBootstrapper {
+    return this._extensionBootstrapper;
+  }
+
+  public get env(): string {
+    return this._env;
+  }
+
+  public get configPath(): string {
+    return this._configPath;
+  }
+
+  protected initializeLogging(): void {
+  }
+
+  public async initialize(): Promise<void> {
+    if (!this.isInitialized) {
+      this.initializeLogging();
+
+      configureAddictIocWithNconf(this.container, {configPath: this.configPath, env: this.env});
+
+      await this.extensionBootstrapper.initialize();
+      this.isInitialized = true;
+    }
+  }
+
+  public async start(): Promise<void> {
+
+    await this.initialize();
+    await this.extensionBootstrapper.start();
+
+  }
+
+}
