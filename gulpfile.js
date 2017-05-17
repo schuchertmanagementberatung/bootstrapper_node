62fb99c17cfd7a015fe193bfc6498d7e58c52187
Simon Reymann <simon.reymann@gmail.com>
2017-05-17 11:30:46 +0200

    build dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
 'use strict';
 
 const gulptraum = require('gulptraum');
+const tsconfig = require('tsconfig');
 
 const buildSystemConfig = {
-  packageName: 'core'
 };
 
 const buildSystem = new gulptraum.BuildSystem(buildSystemConfig);
 
 buildSystem.config = buildSystemConfig;
 
-const typeScriptConfig = {
-  compileToModules: ['commonjs'],
-};
+const tsConfigObj = tsconfig.loadSync('.');
+
+const typeScriptConfig = Object.assign({
+  compileToModules: ['commonjs']
+}, tsConfigObj.config);
 
 const gulp = require('gulp');
 
--- src/app_bootstrapper.ts
+++ src/app_bootstrapper.ts
@@ -1,13 +1,8 @@
 import {DependencyInjectionContainer as Container} from 'addict-ioc';
 import {configureAddictIocWithNconf as configureAddictIocWithNconf} from 'addict-ioc-nconf';
-import * as bluebirdPromise from 'bluebird';
 import * as path from 'path';
-import * as util from 'util';
 import {ExtensionBootstrapper} from '@process-engine-js/bootstrapper';
 import {IFactory} from '@process-engine-js/core_contracts';
-
-import * as createLogger from 'loggerhythm';
-const logger = createLogger('bootstrapper');
 
 export const extensionDiscoveryTag: string = 'extension';
 
@@ -61,6 +56,7 @@ export class AppBootstrapper {
   }
 
   protected initializeLogging(): void {
+    return;
   }
 
   public async initialize(): Promise<void> {
