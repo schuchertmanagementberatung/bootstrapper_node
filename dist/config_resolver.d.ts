7fd141192abfcb2d4f23d0cffc8a974599730217
Heiko Mathes <heiko.mathes@gmail.com>
2017-09-08 17:24:02 +0200

    :fire: remove dist-folder

--- dist/config_resolver.d.ts
+++
@@ -1,7 +0,0 @@
-import { IInstanceWrapper, Resolver } from 'addict-ioc';
-export declare class ConfigResolver extends Resolver<any, IInstanceWrapper<any>> {
-    private _nconf;
-    constructor(nconf: any);
-    readonly nconf: any;
-    resolveConfig(configNamespace: any): any;
-}
